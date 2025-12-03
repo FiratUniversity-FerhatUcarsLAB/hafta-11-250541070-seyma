0b28b83710ecffbc333c7f7eeb8afdd4b0b03d90
250541070-seyma <250541070@firat.edu.tr>
2025-12-03 21:37:58 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,25 @@
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(day + " " + date + " " + month + " " + year);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Wednesday";
+        int date = 3;
+        String month = "December";
+        int year = 2025;
+
+        System.out.println("American Format:");
+        printAmerican(day, date, month, year);
+
+        System.out.println();
+
+        System.out.println("European Format:");
+        printEuropean(day, date, month, year);
     }
 }
